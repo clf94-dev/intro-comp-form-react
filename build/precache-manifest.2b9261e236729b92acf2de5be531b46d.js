aca9b4d6764423e6ecfa2bd9ac8f7e49a7175540
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-19 22:06:57 +0200

    Add files via upload

---
+++ build/precache-manifest.2b9261e236729b92acf2de5be531b46d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eae93ef2b7fc80d3bb96e962aba985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db516ad23afdff4fafbc",
+    "url": "/static/css/main.00ddbc13.chunk.css"
+  },
+  {
+    "revision": "658addfb5a8e9653a0df",
+    "url": "/static/js/2.2ecee29b.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.2ecee29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db516ad23afdff4fafbc",
+    "url": "/static/js/main.28f20a79.chunk.js"
+  },
+  {
+    "revision": "d236aa8cbb0f72138098",
+    "url": "/static/js/runtime-main.d399a76a.js"
+  },
+  {
+    "revision": "eb8c9c1625fcb08ca7bfc046f015ea87",
+    "url": "/static/media/bg-intro-desktop.eb8c9c16.png"
+  },
+  {
+    "revision": "3e78c71a68ca0575e5ffb30e2e8ccc69",
+    "url": "/static/media/bg-intro-mobile.3e78c71a.png"
+  }
+]);
